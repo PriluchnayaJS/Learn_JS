90deda59603506cbdff0eede3e8e7b05cbd3d44a
Priluchnaya Olga <ob261264@gmail.com>
2020-10-14 01:04:31 +0300

    внесены пустые строки-разделители

--- script/script.js
+++ script/script.js
@@ -2,10 +2,14 @@ const period = "два месяца";
 alert('Курс по JavaScript будет длиться ' + period);
 let money = 20.5;
 console.log('money = ', money);
+
 let income = 70000;
 console.log(`Ваш доход: ${income} руб`);
+
 let addExpenses = 20000;
 const deposit = 1000000;
+
 console.log('Депозит - Затраты = ', deposit - addExpenses);
+
 const mission = 1;
 console.log("Выполнить задание ", mission);
