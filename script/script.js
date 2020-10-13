8b11b0591f07e6b697318ed8cf5b3aa68697975a
Priluchnaya Olga <ob261264@gmail.com>
2020-10-14 00:50:03 +0300

    добавлены пустые разделительные строки

--- script/script.js
+++ script/script.js
@@ -10,13 +10,18 @@ let amount1 = prompt('Во сколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов:');
 let amount2 = prompt('Во сколько это обойдется?');
 let budgetMonth = money - (parseInt(amount1) + parseInt(amount2));
+
 console.log('Бюджет на месяц: ', budgetMonth);
+
 const mission = 500000;
 console.log("Цель заработать ", mission, " рублей");
+
 let period = Math.ceil(mission / budgetMonth);
 console.log('Цель будет достигнута через ', period, '  месяцев');
+
 let budgetDay = budgetMonth / 30;
 console.log('Дневной бюджет', Math.floor(budgetDay), 'руб');
+
 if (budgetDay >= 1200) {
     console.log('У Вас высокий уровень дохода');
 } else if (budgetDay >= 600) {
